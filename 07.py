2f18d9ccd116b8b719b38a4b37008947f179e4dd
Dan Pfister <dan.pfister27@gmail.com>
2022-11-03 10:19:43 +0100

    solved day 7 task 2

--- 07.py
+++ 07.py
@@ -1,5 +1,6 @@
 import numpy as np
 import re
+from scipy.special import comb
 
 file = open(r'input\07.txt', 'r')
 line = file.readline()
@@ -7,8 +8,20 @@ inputdata = np.asarray(re.split(r'\D', line)[:-1], dtype=np.int32)
 
 ############################## PART 1 ##############################
 
-median = np.median(inputdata)
+fuelarray = np.zeros((1000), dtype=np.int32)
+indices = np.arange(1000)
 
-fuelcost = np.abs(inputdata - median)
+for input in inputdata:
+    fuelarray = np.where(fuelarray != input, fuelarray + abs(indices - input), 0)
 
-print(f"total fuel cost: {np.sum(fuelcost)}")
+print(f"total fuel cost of task 1 is {np.min(fuelarray)} at position {np.argwhere(fuelarray == np.min(fuelarray))[0][0]}")
+
+############################## PART 2 ##############################
+
+fuelarray = np.zeros((1000), dtype=np.int32)
+indices = np.arange(1000)
+
+for input in inputdata:
+    fuelarray = np.where(fuelarray != input, fuelarray + comb(abs(indices - input)+1, 2), 0)
+
+print(f"total fuel cost of task 2 is {int(np.min(fuelarray))} at position {np.argwhere(fuelarray == np.min(fuelarray))[0][0]}")
