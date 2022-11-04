57e85ab222040cebcc7b04b677a676296401c5f3
Dan <dan.pfister27@gmail.com>
2022-11-04 16:05:01 +0100

    work in progress

--- 08.py
+++ 08.py
@@ -30,3 +30,33 @@ for entry in inputdata:
                 pass
 
 print(f"Number of occurences of digits 1, 4, 7, 8: {np.sum(numberOfDigits)}")
+
+############################## PART 2 ##############################
+
+letters = 'abcdefg'
+
+for entry in inputdata:
+    configuration = {}
+    lettercount = {}
+    # count occurence of each signal
+    for letter in letters:
+        count = 0
+        for index in range(10):
+            if letter in entry[index]:
+                count += 1
+        lettercount[letter] = count
+    # signal with 4 occurences is segment 4,
+    # signal with 6 occurences is segment 1,
+    # signal with 8 occurences is segment 2
+    # and signal with 9 occurences is segment 5
+    for signal, count in lettercount.items():
+        match count:
+            case 4:
+                configuration[signal] = 4
+            case 6:
+                configuration[signal] = 1
+            case 9:
+                configuration[signal] = 5
+            case _:
+                pass
+    print(configuration )
